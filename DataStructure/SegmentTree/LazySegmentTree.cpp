3b4b805e878300a1d792dba232dc5cf943e18a94
dechancer <dechancer@163.com>
2024-03-13 13:17:09 +0800

    SparseTable added. SegmentTree function updated.

--- DataStructure/SegmentTree/LazySegmentTree.cpp
+++ DataStructure/SegmentTree/LazySegmentTree.cpp
@@ -5,23 +5,21 @@ struct LazySegmentTree {
   Tag v;
   vector<Info> seg;
   vector<Tag> tag;
-
-  LazySegmentTree(const vector<Info>& a) : LazySegmentTree(a.size()) {
-    function<void(int, int, int)> build = [&](int l, int r, int p) {
-      if (l == r)
-        return void(seg[p] = a[l - 1]);
-
-      int mid = (l + r) / 2;
-      build(l, mid, p * 2);
-      build(mid + 1, r, p * 2 + 1);
-      pull(p);
-    };
-    build(1, n, 1);
-  }
-
   LazySegmentTree(int n) : n(n) {
     seg.resize(n * 4);
     tag.resize(n * 4);
+  }
+  LazySegmentTree(const vector<Info>& a) : LazySegmentTree(a.size()) {
+    function<void(int, int, int)> build = [&](int l, int r, int p) {
+      if (l + 1 == r)
+        return void(seg[p] = a[l]);
+
+      int mid = (l + r) / 2;
+      build(l, mid, p * 2);
+      build(mid, r, p * 2 + 1);
+      pull(p);
+    };
+    build(0, n, 1);
   }
 
   void pull(int p) { seg[p] = seg[p * 2] + seg[p * 2 + 1]; }
@@ -39,10 +37,10 @@ struct LazySegmentTree {
     this->ql = ql;
     this->qr = qr;
     this->v = v;
-    update(1, n, 1);
+    update(0, n, 1);
   }
   void update(int l, int r, int p) {
-    if (qr < l or r < ql)
+    if (qr <= l or r <= ql)
       return;
     if (ql <= l and r <= qr)
       return apply(p, v);
@@ -50,24 +48,24 @@ struct LazySegmentTree {
     push(p);
     int mid = (l + r) / 2;
     update(l, mid, p * 2);
-    update(mid + 1, r, p * 2 + 1);
+    update(mid, r, p * 2 + 1);
     pull(p);
   }
 
   Info query(int ql, int qr) {
     this->ql = ql;
     this->qr = qr;
-    return query(1, n, 1);
+    return query(0, n, 1);
   }
   Info query(int l, int r, int p) {
-    if (qr < l or r < ql)
+    if (qr <= l or r <= ql)
       return Info();
     if (ql <= l and r <= qr)
       return seg[p];
 
     int mid = (l + r) / 2;
     push(p);
-    return query(l, mid, p * 2) + query(mid + 1, r, p * 2 + 1);
+    return query(l, mid, p * 2) + query(mid, r, p * 2 + 1);
   }
 };
 
--- DataStructure/SegmentTree/SegmentTree.cpp
+++ DataStructure/SegmentTree/SegmentTree.cpp
@@ -4,54 +4,53 @@ struct SegmentTree {
   int ql, qr;
   Info v;
   vector<Info> seg;
-
+  SegmentTree(int n) : n(n) { seg.resize(n * 4); }
   SegmentTree(const vector<Info>& a) : SegmentTree(a.size()) {
     function<void(int, int, int)> build = [&](int l, int r, int p) {
-      if (l == r)
-        return void(seg[p] = a[l - 1]);
+      if (l + 1 == r)
+        return void(seg[p] = a[l]);
 
       int mid = (l + r) / 2;
       build(l, mid, p * 2);
-      build(mid + 1, r, p * 2 + 1);
+      build(mid, r, p * 2 + 1);
       pull(p);
     };
-    build(1, n, 1);
+    build(0, n, 1);
   }
-
-  SegmentTree(int n) : n(n) { seg.resize(n * 4); }
 
   void pull(int p) { seg[p] = seg[p * 2] + seg[p * 2 + 1]; }
 
   void update(int i, const Info& v) {
-    ql = qr = i;
+    ql = i;
+    qr = i + 1;
     this->v = v;
-    update(1, n, 1);
+    update(0, n, 1);
   }
   void update(int l, int r, int p) {
-    if (qr < l or r < ql)
+    if (qr <= l or r <= ql)
       return;
-    if (l == r)
+    if (l + 1 == r)
       return seg[p].apply(v);
 
     int mid = (l + r) / 2;
     update(l, mid, p * 2);
-    update(mid + 1, r, p * 2 + 1);
+    update(mid, r, p * 2 + 1);
     pull(p);
   }
 
   Info query(int ql, int qr) {
     this->ql = ql;
     this->qr = qr;
-    return query(1, n, 1);
+    return query(0, n, 1);
   }
   Info query(int l, int r, int p) {
-    if (qr < l or r < ql)
+    if (qr <= l or r <= ql)
       return Info();
     if (ql <= l and r <= qr)
       return seg[p];
 
     int mid = (l + r) / 2;
-    return query(l, mid, p * 2) + query(mid + 1, r, p * 2 + 1);
+    return query(l, mid, p * 2) + query(mid, r, p * 2 + 1);
   }
 };
 
