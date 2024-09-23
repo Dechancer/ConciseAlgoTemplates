f4b386176034b427c6c96e8b5b971476d006eecc
dechancer <dechancer@163.com>
2024-09-23 17:58:27 +0800

    Checker updated. Linux/Bash is now supported.

--- Others/Checker.cpp
+++ Others/Checker.cpp
@@ -1,3 +1,4 @@
+// Windows/Cmd
 struct Checker {
   Checker() {
     system("mkdir exe > nul");
@@ -21,6 +22,41 @@ struct Checker {
       system("exe\\std < txt/gen > txt/std");
       system("exe\\test < txt/gen > txt/test");
       if (system("fc txt\\std txt\\test > nul")) {
+        cout << "TC " << tc++ << " : WA\n\n";
+        output();
+        break;
+      } else {
+        cout << "TC " << tc++ << " : AC\n\n";
+      }
+    }
+  }
+};
+
+// Linux/Bash
+// use "./{progName}" to run complied checker
+struct Checker {
+  Checker() {
+    system("mkdir obj");
+    system("mkdir txt");
+    system("g++ gen.cpp -Ofast -std=c++20 -o obj/gen");
+    system("g++ std.cpp -Ofast -std=c++20 -o obj/std");
+    system("g++ test.cpp -Ofast -std=c++20 -o obj/test");
+  }
+  void output() {
+    cout << "\ngen:\n";
+    system("cat txt/gen");
+    cout << "\nstd:\n";
+    system("cat txt/std");
+    cout << "\ntest:\n";
+    system("cat txt/test");
+  }
+  void run() {
+    int tc = 0;
+    while (true) {
+      system("obj/gen > txt/gen");
+      system("obj/std < txt/gen > txt/std");
+      system("obj/test < txt/gen > txt/test");
+      if (system("diff txt/std txt/test")) {
         cout << "TC " << tc++ << " : WA\n\n";
         output();
         break;
